cb96742f2a0bb128811acb8e3f328df74df9316d
DaniPinelli <danielpinelli.fullstack@gmail.com>
2021-06-15 16:07:32 -0300

    Still working on form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,28 +1,64 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Form = () => {
+  //Add Appointment State
+  const [date, updateDate] = useState({
+    name: "",
+    lastName: "",
+    date: "",
+    time: "",
+    symptoms: "",
+  });
+
+  const updateState = (e) => {
+    updateDate({
+      ...date,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <Fragment>
-      <h2>Create Appointment</h2>
+      <h2>Create </h2>
 
       <form>
         <label>Patient Name</label>
         <input
           type="text"
-          name="patient"
+          name="name"
           className="u-full-width"
           placeholder="First Name"
+          onChange={updateState}
         />
         <input
           type="text"
-          name="patient"
+          name="lastName"
           className="u-full-width"
           placeholder="Last Name"
+          onChange={updateState}
         />
         <label>Date</label>
-        <input type="date" name="date" className="u-full-width" />
+        <input
+          type="date"
+          name="date"
+          className="u-full-width"
+          onChange={updateState}
+        />
         <label>Time</label>
-        <input type="time" name="time" className="u-full-width" />
+        <input
+          type="time"
+          name="time"
+          className="u-full-width"
+          onChange={updateState}
+        />
+        <label>Symptoms</label>
+        <textarea
+          className="u-full-width"
+          name="symptoms"
+          onChange={updateState}
+        ></textarea>
+        <button type="submit" className="u-full-width button-primary">
+          Submit
+        </button>
       </form>
     </Fragment>
   );
